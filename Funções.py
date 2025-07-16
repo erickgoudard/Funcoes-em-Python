0d9feec4890d8eb2e674220b2a33a7c53affd845
Erick <erickgoud19@gmail.com>
2025-07-16 20:53:10 -0300

    Add files via upload

---
+++ Funções.py
@@ -0,0 +1,15 @@
+# Função Saudacao()
+nome = "Henrique"
+print(f"\nOlá, {nome}! Seja bem-vindo(a)!")
+# Função Calcular_Média()
+notas = [7.5, 8, 6.5]
+media = sum(notas) / len(notas)
+print(f"\nMédia: {media:.2f}")
+if media >= 7:
+    print("Aluno aprovado.")
+#Função maior_menor
+numeros = [10, -3, 25, 7]
+maior = max(numeros)
+menor = min(numeros)
+print(f"\nMaior número: {maior}")
+print(f"Menor número: {menor}")
